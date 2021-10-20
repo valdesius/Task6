f0ab846825f30c3f8466fd7ec89bccb5525ad57d
valdesius <v.inc@inbox.ru>
2021-10-20 23:25:21 +0300

    v1

--- src/valdes/inc/com/company/Main.java
+++ src/valdes/inc/com/company/Main.java
@@ -1,44 +1,26 @@
 package valdes.inc.com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-//        pow(1);
-//        calculateExpressionsModulo(5);
-        xInPow(2, 3);
+        int x = readXAndNAndE("Input x: ");
+        int n = readXAndNAndE("Input n: ");
     }
+    private static int readXAndNAndE(String text){
+        int x = 0;
+        Scanner scanner = new Scanner(System.in);
 
-    private static int pow(){
-        double result = 0;
-        for (int i = 0; i <= 6; i++) {
-            System.out.println(result);
-            result += 1;
-
-        }
-        return (int) result;
-
-    }
-
-    private static int calculateExpressionsModulo(int c){
-        int result = 0;
-        int startNum1 = 1;
-        int startNum2 = 2;
-        final int denominator = 2;
-
-        for (int i = 1; i < 9; i++){
-            result  = (startNum1 * startNum2) / denominator;
-            System.out.println(result);
-            startNum1 = startNum1 + 1;
-            startNum2 = startNum1 + 1;
-        }
-        return result;
-
-    }
-    private static int xInPow(int x, int index){
-
-        for (int i = 0; i< 6 ; i++){
-            pow();
+        try {
+            System.out.print("Input  ");
+            x = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.print("Неверно введенные данные ");
+            System.exit(1);
         }
         return x;
     }
+
+
 }
