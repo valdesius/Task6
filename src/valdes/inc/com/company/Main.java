a4614d3e33b274efe086500deb7066c027f69b57
valdesius <v.inc@inbox.ru>
2021-10-24 23:02:13 +0300

    v2.1

--- src/valdes/inc/com/company/Main.java
+++ src/valdes/inc/com/company/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         int x = readXAndNAndE("Input x: ");
         int n = readXAndNAndE("Input n: ");
+        calculateSumOfSequence(x, n);
     }
+
     private static int readXAndNAndE(String text){
         int x = 0;
         Scanner scanner = new Scanner(System.in);
@@ -21,6 +23,27 @@ public class Main {
         }
         return x;
     }
+    //1 пункт
+    private static int calculateSumOfSequence(int x, int n){
+        int growXInPow = 0;
+        int growNumber = 1;
+        int sumOfSequence = 0;
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 0; j <= n; j++) {
+                System.out.println(growXInPow);
+                growXInPow += Math.pow(x, j);
+            }}
+
+
+
+        sumOfSequence = growNumber * growXInPow;
+        System.out.println(sumOfSequence);
+
+        return 0;
+    }
+
+
 
 
 }
