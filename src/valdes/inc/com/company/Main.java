94f421c1b621c453a9f55b4e26652da52f1fa93c
valdesius <v.inc@inbox.ru>
2021-10-26 23:19:23 +0300

    v3.1

--- src/valdes/inc/com/company/Main.java
+++ src/valdes/inc/com/company/Main.java
@@ -7,10 +7,17 @@ public class Main {
     public static void main(String[] args) {
         int x = readXAndNAndE("Input x: ");
         int n = readXAndNAndE("Input n: ");
+        int e = readXAndNAndE("Input e: ");
 
         double sumOfSequence = calculateSumOfSequence(x, n);
-        System.out.println("1. сумма n слагаемых заданного вида = " + sumOfSequence);
+        double sumOfSequenceMoreThanEpsilon = calculateSumOfSequenceMoreThanEpsilon(x, n, e);
+        double sumOfSequenceMoreThanEpsilonDivideTen = calculateSumOfSequenceMoreThanEpsilon(x, n, e / 10);
+        double functionValue = calculateValueOfFunction(x);
 
+        System.out.println("1. сумма n слагаемых заданного вида = " + sumOfSequence);
+        System.out.println("2. сумма тех слагаемых, которые по абсолютной величине больше e = " + sumOfSequenceMoreThanEpsilon);
+        System.out.println("3. сумму тех слагаемых, которые по абсолютной величине больше e/10 = " + sumOfSequenceMoreThanEpsilonDivideTen);
+        System.out.println("4. значение функции с помощью методов Math = " + functionValue);
     }
 
     private static int readXAndNAndE(String text){
@@ -24,6 +31,7 @@ public class Main {
             System.out.print("Not correct data... ");
             System.exit(1);
         }
+
         return x;
     }
 
@@ -40,6 +48,28 @@ public class Main {
 
             growNumber += 1;
         }
+
         return sumOfSequence;
+    }
+
+    private static int calculateSumOfSequenceMoreThanEpsilon(double x, int n, double e){
+        int sumOfSequenceMoreThanEpsilon = 0;
+
+        for (int i = 1; i <= n; i++) {
+            double nMemberOfSequence = getNMemberOfSequence(x, i);
+            if (Math.abs(nMemberOfSequence) > e) {
+                sumOfSequenceMoreThanEpsilon += getNMemberOfSequence(x, i);
+            }
+        }
+
+        return sumOfSequenceMoreThanEpsilon;
+    }
+
+    private static double getNMemberOfSequence(double x, int n) {
+        return Math.pow(x, n - 1);
+    }
+
+    private static double calculateValueOfFunction(double x){
+        return 1 / Math.pow((1 - x), 2);
     }
 }
